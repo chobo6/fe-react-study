0e384a2b804b15959f9c2680f85902d61c3c351a
chobo6 <hongjegue@naver.com>
2026-03-10 16:30:29 +0900

    [React Spring] JWT 방식 로그인 처리

--- src/rs/Login.js
+++ src/rs/Login.js
@@ -66,6 +66,79 @@ function Login(){
                 }}>로그인여부확인버튼</button>
             </div>
 
+            
+            <div>
+                <button onClick={()=>{
+                    //로그인 시도 
+                    //id,pw -> 서버로 로그인 요청 전달
+
+                    axios.post('/api/loginJWT',
+                    {               //post http 요청에 담아서 보낼 body  데이터   json  key:value
+                        id: id,
+                        pw: pw      //pw 키값 : pw state변수값
+                    },
+                    {
+                        headers : {
+                            'Content-Type':'application/json'
+                        }
+                    }
+                ).then(
+                    (response)=>{
+                        console.log(response.data);  // JWT accessToken
+                        
+                        let token = response.data;  //JWT token
+
+                        //관리 -> 계속 인식되도록 보관 -> localStorage 브라우저
+                        
+                        //state -> props 
+                        //Redux 
+                        //localStorage
+                        //cookie
+                        
+                        if(token != null && token != '') { //단순텍스트 처리
+                            //발급된 토큰이 수신됐다.
+                            // 잘 저장 -> 다음에 요청할때 토큰같이 담아서 요청 (ex 자유이용권 제시)
+
+                            // 토큰 -> localstorage 저장
+                            // localStorage.setItem(key, value)
+                            // localStroage.getItem(key)
+                            localStorage.setItem('token', token);
+                        }
+
+                        //로그인 성공 or 실패?
+                        // 다음 페이지 전환처리 ...  실패메시지 ... 
+
+
+                        //apiResponse -> header:resultCode   body:{accessToken:"asodjfoiasjfiasjfasijfo"}
+
+                    }
+                )
+                .catch(error => { console.log(error) })
+
+                }}>로그인JWT방식</button>
+                
+                <button onClick={()=>{
+
+                    
+                    let token = localStorage.getItem('token');
+
+                    axios.post(
+                        '/api/loginCheckJWT',
+                        {},
+                        {
+                            headers :{
+                                'Content-Type':'application/json',
+                                'Authorization' : "Bearer " + token
+                            }
+                        }
+                    )
+                    .then(response => console.log(response.data))
+                    .catch(error => console.log(error))
+
+                }}>로그인여부확인버튼JWT방식</button>
+            </div>
+
+
         </div>
         
     )
